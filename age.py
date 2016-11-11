89db5057d33a2c44a2b68b80604c68a02a2dd259
ams103 <ams_103@hotmail.com>
2016-11-11 09:47:28 +0100

    First edit

---
+++ age.py
@@ -0,0 +1,44 @@
+import os
+print("Enter your birthday to find out how long you have lived in days, and  total hours")
+a = int(input("Enter your birthday, first enter your year "))
+b = int(input("Enter your month "))
+c = int(input("Enter your day "))
+
+import datetime
+today = datetime.date.today()
+birthday = datetime.date(a,b,c)
+diff = birthday - today
+a= diff.days
+b= -a
+print("Today you have lived ",b," days or", (b*24)," hours")
+
+from datetime import datetime, timedelta
+
+d = datetime.today() - timedelta (days=-1000)
+print("In 1000 days the date would be", d)
+
+import datetime
+now = datetime.datetime.now()
+print("-"*22)
+print(now)
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+
+print("-"*25)
+print("1 week ago was it: ", (now - datetime.timedelta(weeks=1)))
+print("100 days ago was: ",(now - datetime.timedelta(days=100)))
+print("1 week from now is it: ",(now + datetime.timedelta(weeks=1)))
+print("In 1000 days from now is it: ",(now + datetime.timedelta(days=1000)))
+
+print("-"*25)
+birthday = datetime.datetime(2017,1,31)
+longlived = datetime.datetime(1994,1,31)
+print("Birthday in ", (birthday - now))
+print("You have lived", (now - longlived))
+print("-"*25)
+
+os.system("pause")
